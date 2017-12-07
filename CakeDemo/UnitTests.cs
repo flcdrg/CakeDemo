7f62e664c47e5cbca9898522361e2ba090084426
David Gardiner <david@gardiner.net.au>
2017-12-07 21:18:06 +1030

    NUnit tests

---
+++ CakeDemo/UnitTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CakeDemo
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void Addition()
+        {
+            Assert.AreEqual(4, 2 + 2);
+        }
+
+        [Test]
+        public void Multiplication()
+        {
+            Assert.AreEqual(4, 2 * 2);
+        }
+    }
+}
