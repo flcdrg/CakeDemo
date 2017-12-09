9489191a75b5701dfacf69150742d6da4900c7cc
David Gardiner <david@gardiner.net.au>
2017-12-10 09:35:51 +1030

    Add WriteArgumentsEvent method

--- CakeDemoAddin/UtilityExtension.cs
+++ CakeDemoAddin/UtilityExtension.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Diagnostics;
 using Cake.Core;
 
 namespace CakeDemoAddin
 {
-    public class UtilityExtension
+    public static class UtilityExtension
     {
+        public static void WriteArgumentsEvent(this ICakeContext context, string source, string message)
+        {
+            if (!EventLog.SourceExists(source))
+                EventLog.CreateEventSource(source, "Application");
+
+            EventLog.WriteEntry(source, message, EventLogEntryType.Information);
+        }
     }
 }
